51b82b6b88185cce6d9f20c2ff08a78fb654d1d2
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-10-16 20:44:42 +0200

    PluginStore: Add new plugin type "TTSPlugin"

--- client/pluginstore.py
+++ client/pluginstore.py
@@ -150,6 +150,7 @@ class PluginStore(object):
         self._translations_dirname = PLUGIN_TRANSLATIONS_DIRNAME
         self._categories_map = {
             'speechhandler': plugin.SpeechHandlerPlugin,
+            'tts': plugin.TTSPlugin,
             'stt': plugin.STTPlugin
         }
 
